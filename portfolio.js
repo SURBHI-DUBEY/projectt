319fe992c76eccf468d89b64676d7dfa688dd324
SURBHI-DUBEY <surbhid.14@gmail.com>
2024-01-06 01:13:09 +0530

    added new file and modified content in html/css and js

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+/*-----about section -----*/
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(event,tabname) {
+  for (var i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active-link");
+  }
+
+  for (var i = 0; i < tabcontents.length; i++) {
+    tabcontents[i].classList.remove("active-tab");
+  }
+
+  if (event) {
+    event.currentTarget.classList.add("active-link");
+  }
+
+  document.getElementById(tabname).classList.add("active-tab");
+}
